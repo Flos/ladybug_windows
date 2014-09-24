e14c643ebab051c6a0a11515b6107ff1f97224cf
Florian Nolden <florian.nolden@googlemail.com>
2014-09-25 01:30:30 +0200

    service: added polling to zmq

--- service/zmq_service.h
+++ service/zmq_service.h
@@ -5,10 +5,18 @@
 #include <google/protobuf/message.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <imageMessage.pb.h>
+#include <string>
+#include <stdexcept>
+
+#define REQUEST_TIMEOUT 10 // msecs, (> 1000!)
+#define REQUEST_RETRIES 3 // Before we abandon
 
 class Zmq_service
 {
 public:
+	std::string connection;
+	int type;
+
 	Zmq_service(void);
 	void init(std::string socket, int type);
 
@@ -25,8 +33,12 @@ public:
 	bool receive(ladybug5_network::pbMessage& pb_msg, int flag = 0);
 	bool receive(zmq::message_t &msg, int flag = 0);
 
+	void reset_state();
+
 	~Zmq_service(void);
 private:
+	void create_socket(int type);
+	int retries_left;
 	zmq::context_t*zmq_context; 
 	zmq::socket_t* zmq_socket;
 };
